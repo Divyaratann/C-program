b2017e4b8d469a42ab7a1fa8d9132ef783bea97c
Divyaratann srivastava <divyratan2703@gmail.com>
2025-10-08 19:13:45 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+   
+   //clrscr();
+   float Area,a,b;
+    printf("enter a value of a and b to calculate its area of circle \n");
+    scanf("%f%f", &a,&b);
+    {
+        Area= a*b;
+        printf("Area=""%f",Area);
+    }
+    getch();
+}    
+    
+    
+    
+    
+    
+    
